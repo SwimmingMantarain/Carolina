a7630f297f091d7f114cc331aaee0bbea597d2c0
SwimmingMantarain <swimmingmantarain@gmail.com>
2025-09-05 09:50:32 +0200

    line!

--- kernel/src/framebuffer.zig
+++ kernel/src/framebuffer.zig
@@ -2,34 +2,64 @@ const limine = @import("limine");
 const serial = @import("./serial.zig");
 
 export var framebuffer_request: limine.FramebufferRequest linksection(".limine_requests") = .{};
+var framebuffer: *limine.Framebuffer = undefined;
+var fb_ptr: [*]volatile u32 = undefined;
 
 fn get_framebuffer() *limine.Framebuffer {
     if (framebuffer_request.response) |framebuffer_response| {
-        const framebuffer = framebuffer_response.getFramebuffers()[0];
-        return framebuffer;
+        return framebuffer_response.getFramebuffers()[0];
     } else {
         @panic("Framebuffer response not present");
     }
 }
 
+pub fn draw_pixel(x: u32, y: u32, color: u32) void {
+    if (x > framebuffer.width) {
+        return;
+    } else if (y > framebuffer.height) {
+        return;
+    } else {
+        fb_ptr[x + y * framebuffer.width] = color;
+    }
+}
+
+pub fn draw_line(x1: u32, y1: u32, x2: u32, y2: u32, color: u32) void {
+    const slope = 2 * (y2 - y1);
+    var slope_error = slope - (x2 - x1);
+
+    var xn = x1;
+    var yn = y1;
+
+    while (xn <= x2) : (xn += 1) {
+        draw_pixel(xn, yn, color);
+
+        slope_error += slope;
+
+        if (slope_error >= 0) {
+            yn += 1;
+            slope_error -= 2 * (x2 - x1);
+        }
+    }
+}
+
+pub fn draw_rect(x1: u32, y1: u32, x2: u32, y2: u32, color: u32) void {
+    draw_line(x1, y1, x1, y2, color); // left
+    draw_line(x1, y1, x2, y1, color); // top
+    draw_line(x2, y1, x2, y2, color); // right
+    draw_line(x1, y2, x2, y2, color); // bottom
+}
+
 pub fn init() void {
-    const framebuffer = get_framebuffer();
+    framebuffer = get_framebuffer();
+    fb_ptr = @ptrCast(@alignCast(framebuffer.address));
 
-    const framebuffer_width = framebuffer.width;
-    const framebuffer_height = framebuffer.height;
-    const framebuffer_pitch = framebuffer.pitch;
-    const framebuffer_bpp = framebuffer.bpp;
+    const framebuffer_length = framebuffer.width * framebuffer.height;
 
-    serial.outNum(0xe9, framebuffer_width);
+    serial.outNum(0xe9, framebuffer.width);
     serial.outb(0xe9, 'x');
-    serial.outNum(0xe9, framebuffer_height);
-    serial.outb(0x09, ' ');
-    serial.outNum(0xe9, framebuffer_pitch);
-    serial.outb(0xe9, ' ');
-    serial.outNum(0xe9, framebuffer_bpp);
+    serial.outNum(0xe9, framebuffer.height);
 
-    for (0..framebuffer_width) |i| {
-        const fb_ptr: [*]volatile u32 = @ptrCast(@alignCast(framebuffer.address));
-        fb_ptr[i] = 0x00ff00;
+    for (0..framebuffer_length) |i| {      
+        fb_ptr[i] = 0x000000;
     }
 }
--- kernel/src/main.zig
+++ kernel/src/main.zig
@@ -7,6 +7,12 @@ inline fn hlt() void {
 export fn _start() callconv(.C) noreturn {
     framebuffer.init();
 
+    framebuffer.draw_line(100, 100, 100, 200, 0x00ff00);
+    framebuffer.draw_line(100, 100, 200, 99, 0x00ff00);
+
+
+    //framebuffer.draw_rect(200, 250, 500, 600, 0xff00ff);
+
     while (true) {
         hlt();
     }
